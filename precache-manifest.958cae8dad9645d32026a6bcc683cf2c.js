0f12af899921a3b4302bf7e168f82502df2bcf7b
Kotelnikov Igor <digikot1996@gmail.com>
2019-11-29 14:37:06 +0200

    Updates

---
+++ precache-manifest.958cae8dad9645d32026a6bcc683cf2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d4334e694e00133a507bc98f90fccc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80a1178da077cf2a9710",
+    "url": "/static/css/main.c19eef94.chunk.css"
+  },
+  {
+    "revision": "09c8467cc3c21ae5fd81",
+    "url": "/static/js/2.2a05e893.chunk.js"
+  },
+  {
+    "revision": "80a1178da077cf2a9710",
+    "url": "/static/js/main.2ba6ec47.chunk.js"
+  },
+  {
+    "revision": "ea8c27f30b2d715c465a",
+    "url": "/static/js/runtime-main.a91ef5ee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.32076366b9dc211d8290a6a252d794b3.js"
+  "/precache-manifest.958cae8dad9645d32026a6bcc683cf2c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-03-image-finder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
